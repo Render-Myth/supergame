403df503ed2c2b61856f04b3a8008edf2852daf0
Render-Myth <cuburulia@gmail.com>
2026-08-09 18:59:31 +0300

    my_work.py

---
+++ my_work.py
@@ -0,0 +1,4 @@
+noun = input ("Введи іменник: ")
+verb = input ("Введи дієслово: ")
+adjective = input ("Введи прикметник: ") 
+print ("Одного разу", adjective, noun, "вирішив", verb, "через паркан.")
